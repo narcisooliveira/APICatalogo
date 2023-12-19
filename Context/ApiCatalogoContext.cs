88a69c902db5c564ad58baa0cf9f4363a2d25618
narcisooliveira <nrcsfo@gmail.com>
2023-12-19 11:33:12 -0300

    Adicionando connectionString, classes nuláveis e o serviço para conexão com o BD.

--- Context/ApiCatalogoContext.cs
+++ Context/ApiCatalogoContext.cs
@@ -8,7 +8,7 @@ namespace APICatalogo.Context
         public ApiCatalogoContext(DbContextOptions<ApiCatalogoContext> options) : base(options)
         {}
 
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApiCatalogoContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
