dc4ba50ea02a22a0854776d52167324e4ee94dd9
narcisooliveira <nrcsfo@gmail.com>
2023-12-19 19:27:14 -0300

    Criando construtor da classe Categoria e inicializando coleção de produtos.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,7 +1,14 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models
 {
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
         public int CategoriaId { get; set; }
         public string? Nome { get; set; }
         public string? ImagemUrl { get; set; }
