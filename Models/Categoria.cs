72dfb18756d66d560e972837534e5e8d24e71bf3
narcisooliveira <nrcsfo@gmail.com>
2023-12-19 10:25:14 -0300

    Add project files.

---
+++ Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+        public ICollection<Produto>? Produtos { get; set; }
+
+    }
+}
