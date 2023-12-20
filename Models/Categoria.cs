42e5b0546a1c7abb5725544be491c161330b218a
narcisooliveira <nrcsfo@gmail.com>
2023-12-20 19:18:20 -0300

    - Adicionando migrações iniciais e de ajustes com EF. - Alterando connectionString. - Adicionando propriedades data annotation.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -9,8 +9,15 @@ namespace APICatalogo.Models
             Produtos = new Collection<Produto>();
         }
 
+        [System.ComponentModel.DataAnnotations.Key]
         public int CategoriaId { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.StringLength(80)]
         public string? Nome { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.StringLength(500)]
         public string? ImagemUrl { get; set; }
         public ICollection<Produto>? Produtos { get; set; }
 
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,11 +1,26 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
     public class Produto
     {
+        [System.ComponentModel.DataAnnotations.Key]
         public int ProdutoId { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Column(TypeName = "varchar(80)")]
         public string? Nome { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Column(TypeName = "varchar(500)")]
         public string? Descricao { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Column(TypeName = "decimal(10, 2)")]
         public decimal Preco { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [Column(TypeName = "varchar(500)")]
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
