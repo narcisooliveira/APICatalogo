1047933b76358b2eb2d362028b41260c4fac2f7c
narcisooliveira <nrcsfo@gmail.com>
2024-01-15 21:24:43 -0300

    Adicionando validações de propriedades na model de produto com data annotations.

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,26 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Models
 {
     public class Produto
     {
-        [System.ComponentModel.DataAnnotations.Key]
+        [Key]
         public int ProdutoId { get; set; }
 
-        [System.ComponentModel.DataAnnotations.Required]
+        [Required]
+        [StringLength(80, ErrorMessage = "O nome deve ter entre 5 e 80 caracteres", MinimumLength = 5)]
         [Column(TypeName = "varchar(80)")]
         public string? Nome { get; set; }
 
-        [System.ComponentModel.DataAnnotations.Required]
+        [Required]
+        [StringLength(300, ErrorMessage = "A descrição deve ter entre 5 e 300 caracteres", MinimumLength = 5)]
         [Column(TypeName = "varchar(500)")]
         public string? Descricao { get; set; }
 
-        [System.ComponentModel.DataAnnotations.Required]
+        [Required]
+        [Range(1, 10000, ErrorMessage = "O preço deve estar entre 1 e 10000 reais")]
         [Column(TypeName = "decimal(10, 2)")]
         public decimal Preco { get; set; }
 
-        [System.ComponentModel.DataAnnotations.Required]
+        [Required]
+        [StringLength(500, ErrorMessage = "A descrição deve ter entre 5 e 500 caracteres", MinimumLength = 5)]
         [Column(TypeName = "varchar(500)")]
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
