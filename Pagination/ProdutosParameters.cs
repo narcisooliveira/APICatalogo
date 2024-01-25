8c078508a44e246cb335c7346fde2685820cadb8
narcisooliveira <nrcsfo@gmail.com>
2024-01-24 22:09:52 -0300

    Adicionando lista paginada para categorias e adicionando logs de erro.

--- Pagination/ProdutosParameters.cs
+++ Pagination/ProdutosParameters.cs
@@ -1,14 +1,7 @@
 ﻿namespace APICatalogo.Pagination
 {
-    public class ProdutosParameters
+    public class ProdutosParameters : QueryStringParameters
     {
-        const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 5;
-        public int PageSize
-        {
-            get { return _pageSize; }
-            set { _pageSize = (value > maxPageSize) ? maxPageSize : value; }
-        }
+
     }
 }
---
+++ Pagination/QueryStringParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace APICatalogo.Pagination
+{
+    public class QueryStringParameters
+    {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 5;
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > maxPageSize) ? maxPageSize : value; }
+        }
+    }
+}
